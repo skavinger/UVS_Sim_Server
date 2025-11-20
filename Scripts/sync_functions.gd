7552199a27ac4295119cbf4052182098aecc786b
skavinger <skavinger@gmail.com>
2025-11-20 10:03:28 -0800

    Add confirmation messaging support

--- Scripts/sync_functions.gd
+++ Scripts/sync_functions.gd
@@ -4,3 +4,13 @@ extends Node
 func syncHealth(_health):
 	if Server.checkPair(multiplayer.get_remote_sender_id()):
 		rpc_id(Server.playerPairs[multiplayer.get_remote_sender_id()], "syncHealth", _health)
+
+@rpc("any_peer")
+func sendMessageToRival(message):
+	if Server.checkPair(multiplayer.get_remote_sender_id()):
+		rpc_id(Server.playerPairs[multiplayer.get_remote_sender_id()], "sendMessageToRival", message)
+
+@rpc("any_peer")
+func sendRivalMessageReply(answer):
+	if Server.checkPair(multiplayer.get_remote_sender_id()):
+		rpc_id(Server.playerPairs[multiplayer.get_remote_sender_id()], "sendRivalMessageReply", answer)
